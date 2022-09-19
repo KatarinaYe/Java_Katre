9f26200b98babd94caf3dc3db6f1b5bc3cfefd7a
Katre Kivaste <katarinaye@gmail.com>
2022-09-19 20:28:34 +0300

    Commit

---
+++ src/main/java/MyProgram.java
@@ -0,0 +1,11 @@
+public class MyProgram {
+
+    public static void main(String[] args) {  // psvm - lühend
+        //sout
+        System.out.println("Hello World");
+        int age = 36;
+        int year = 2022;
+        int birthYear = year - age;
+        System.out.println(birthYear);
+    }
+}
